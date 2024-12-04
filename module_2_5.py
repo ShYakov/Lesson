dccb6ffb6ccbb6fe4a2e72d79149859a4a4d4db3
Shiry <Shiryaokv95@gmail.com>
2024-12-04 21:11:21 +0300

    Comm

--- module_2_5.py
+++ module_2_5.py
@@ -6,8 +6,10 @@ def get_matrix(n, m, value):
             matrix1.append(value)
             matrix.append(matrix1)
         return matrix
-result1 = get_matrix(2,2,10)
-result2 = get_matrix(3,5,42)
-result3 = get_matrix(99,8,13)
 
-print(result1, result2,result3, sep='\n')
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(99, 8, 13)
+
+print(result1, result2, result3, sep='\n')
