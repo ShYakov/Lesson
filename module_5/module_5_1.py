9cd14edbc50f1dfd2e51366f1e8988aaccf89449
Yakov <shiryakov95@gmail.com>
2025-01-03 15:19:44 +0300

    Update module_5_1.py

--- module_5/module_5_1.py
+++ module_5/module_5_1.py
@@ -9,6 +9,10 @@ class House:
                 print(i)
         else:
             print('Такого этажа не существует')
+    def __len__(self):
+        return self.number_of_floor
+    def __str__(self):
+        return self.name
 
 
 
@@ -16,6 +20,16 @@ h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
+print(f'Название: {h1.name}, кол-во этажей: {len(h1)}')
+print(f'Название: {h2.name}, кол-во этажей: {len(h2)}')
+print(len(h1))
+print(len(h2))
+
+
+
+
+
+
 
 
 
